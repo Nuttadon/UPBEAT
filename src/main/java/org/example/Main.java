1665f98393c7ce5c7a0df8e858a1ff1f22460a0a
Nuttadon <nuttadon_p@cmu.ac.th>
2023-02-22 16:06:09 +0700

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws LexicalError, SyntaxError, EvalError, IOException {
+        //        ExprTokenizer t = new ExprTokenizer("m = 5^2 if(10-12)then if(m)then n = nearby up else {} else {n=65}");
+        ExprTokenizer t = new ExprTokenizer("m = 5^2 while(m){m=m-5}");
+        ExprParser p = new ExprParser(t);
+        p.Plan();
+
+    }
+}
