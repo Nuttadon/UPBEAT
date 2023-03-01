3cb1178ffb1e467ea5b172bf1efa53580b673e4b
BezTTTT <Best_1614@hotmail.com>
2023-03-01 19:41:30 +0700

    regionClass adjust

--- src/main/java/org/example/RegionClass.java
+++ src/main/java/org/example/RegionClass.java
@@ -1,7 +1,46 @@
 package org.example;
 
-public class RegionClass {
-    private Double curDeposit = 0.00;
+public class RegionClass implements Region{
     //private Player owner;
     //private Player CityCenter = null;
+    private Boolean isP1CityCenter;
+    private Double curDeposit = 0.00;
+    private Boolean hasOwner = false;
+
+    @Override
+    public void freeOwner() {
+        if(hasOwner.equals(true)){
+            hasOwner = false;
+        }
+    }
+
+    @Override
+    public void freeCityCenter() {
+
+    }
+
+    @Override
+    public void setCityCenter() {
+
+    }
+
+    @Override
+    public void deposit(int i) {
+
+    }
+
+    @Override
+    public void withdrawn(int i) {
+
+    }
+
+    @Override
+    public void depCheck() {
+
+    }
+
+    @Override
+    public void whoOwn() {
+
+    }
 }
--- src/main/java/org/example/TerritoryClass.java
+++ src/main/java/org/example/TerritoryClass.java
@@ -1,4 +1,53 @@
 package org.example;
 
-public class TerritoryClass {
+public class TerritoryClass implements Territory{
+    @Override
+    public void freeOwner() {
+
+    }
+
+    @Override
+    public void freeCityCenter() {
+
+    }
+
+    @Override
+    public void setCityCenter() {
+
+    }
+
+    @Override
+    public void deposit(int i) {
+
+    }
+
+    @Override
+    public void withdrawn(int i) {
+
+    }
+
+    @Override
+    public void depCheck() {
+
+    }
+
+    @Override
+    public void whoOwn() {
+
+    }
+
+    @Override
+    public void winCheck() {
+
+    }
+
+    @Override
+    public void nextTurn() {
+
+    }
+
+    @Override
+    public double calculatePct() {
+        return 0;
+    }
 }
